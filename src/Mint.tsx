cb1cd329d071d284863bdf8b0728e9f2da8d79fa
Zhi Pan <zhilingpan@gmail.com>
2023-08-27 21:20:18 -0400

    type

--- src/Mint.tsx
+++ src/Mint.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import {
   usePrepareContractWrite,
   useContractWrite,
@@ -11,6 +10,8 @@ import appConfigs from '../configs.tsx'
 import ContractAbi from "./abi.ts";
 import { decodeAbiParameters } from 'viem';
 
+type EncodedString = `0x${string}`;
+
 
 const proof = {"merkle_root":"0x2bce521c4b0243bf7104ebd912b92f6773374fe8a4b17099d1ef928e95a32407","nullifier_hash":"0x24c846a2ed4a1b9b05df40502fb0cb43a50379c0186d1d09e615394cb571cc8d","proof":"0x19c6a62778643208c5f9c5eaaf5c7bd93a8a6a63366d8aa9416cfa6253f122011218b30b44789ed9ea8b5ee7abd82415fcc63ce4a3ecaf40412231b09f39508124069cd0667998d2d9a708ea0ede054ef49ea02dfd8496bb0c60d78e98f2f6a90863045dd30e7dfe3d5f73c604d27b71914eb3f89aaa63a023426dcd6b6ab5dc1806b274c9566e1185d71df2eedb9f6bac5f85845f6fd88bd71a4a92746d903b2b7982babf8e8f988b96cfd5d93fd4d9313efe1e5ace995fbdded64fedd2829a08df16f4568a499549abd9dd179ffcc5e37d41f65eaa0f5fd15c852a91d348be1696ca58510485865fed48a03874294e27f22a2eb184f9a37b07959c1bea7f86","credential_type":"orb"}
 
@@ -22,7 +23,7 @@ export default function MintNFT() {
     error: prepareError,
     isError: isPrepareError,
   } = usePrepareContractWrite({
-    address: appConfigs.AIRDROP_CONTRACT_ADDRESS,
+    address: appConfigs.AIRDROP_CONTRACT_ADDRESS as EncodedString,
     abi: ContractAbi,
     functionName: 'verifyAndExecute',
     args: [
@@ -49,7 +50,7 @@ export default function MintNFT() {
 
   return (
     <div>
-      <button disabled={!write || isLoading} onClick={() => write()}>
+      <button disabled={!write || isLoading} onClick={() => write && write()}>
         {isLoading ? 'Minting...' : 'Mint'}
       </button>
       {isSuccess && (
